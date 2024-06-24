9e3c899f2a5807465b75dae5315bde0ed5485e00
asmanda03 <149610069+asmanda03@users.noreply.github.com>
2024-06-24 10:59:11 -0400

    initial commit

---
+++ jokes.js
@@ -0,0 +1,258 @@
+import React from "react";
+import JokeList from "./JokeList";
+
+/** Top-level component. */
+
+function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+}
+
+export default App;
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+registerServiceWorker();
+
+import React from "react";
+import "./Joke.css";
+
+/** Presentational joke component with voting buttons */
+
+function Joke({ vote, votes, text, id }) {
+  function upVote(evt) { vote(id, +1); }
+  function downVote(evt) { vote(id, -1); }
+
+  return (
+    <div className="Joke">
+      <div className="Joke-votearea">
+        <button onClick={upVote}>
+          <i className="fas fa-thumbs-up" />
+        </button>
+
+        <button onClick={downVote}>
+          <i className="fas fa-thumbs-down" />
+        </button>
+
+        {votes}
+      </div>
+
+      <div className="Joke-text">{text}</div>
+    </div>
+  );
+}
+
+export default Joke;
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Joke from "./Joke";
+import "./JokeList.css";
+
+function JokeList({ numJokesToGet = 5 }) {
+  const [jokes, setJokes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  /* get jokes if there are no jokes */
+
+  useEffect(function () {
+    async function getJokes() {
+      let j = [...jokes];
+      let seenJokes = new Set();
+      try {
+        while (j.length < numJokesToGet) {
+          let res = await axios.get("https://icanhazdadjoke.com", {
+            headers: { Accept: "application/json" }
+          });
+          let { ...jokeObj } = res.data;
+
+          if (!seenJokes.has(jokeObj.id)) {
+            seenJokes.add(jokeObj.id);
+            j.push({ ...jokeObj, votes: 0 });
+          } else {
+            console.error("duplicate found!");
+          }
+        }
+        setJokes(j);
+        setIsLoading(false)
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    if (jokes.length === 0) getJokes();
+  }, [jokes, numJokesToGet]);
+
+  /* empty joke list and then call getJokes */
+
+  function generateNewJokes() {
+    setJokes([]);
+    setIsLoading(true);
+  }
+
+  /* change vote for this id by delta (+1 or -1) */
+
+  function vote(id, delta) {
+    setJokes(allJokes =>
+      allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+    );
+  }
+
+  /* render: either loading spinner or list of sorted jokes. */
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <i className="fas fa-4x fa-spinner fa-spin" />
+      </div>
+      )
+  }
+
+  let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
+
+  return (
+    <div className="JokeList">
+      <button className="JokeList-getmore" onClick={generateNewJokes}>
+        Get New Jokes
+        </button>
+
+      {sortedJokes.map(({joke, id, votes}) => (
+        <Joke text={joke} key={id} id={id} votes={votes} vote={vote} />
+      ))}
+    </div>
+  );
+}
+
+export default JokeList;
+
+// In production, we register a service worker to serve assets from local cache.
+
+// This lets the app load faster on subsequent visits in production, and gives
+// it offline capabilities. However, it also means that developers (and users)
+// will only see deployed updates on the "N+1" visit to a page, since previously
+// cached resources are updated in the background.
+
+// To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
+// This link also includes instructions on opting out of this behavior.
+
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === '[::1]' ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+  );
+  
+  export default function register() {
+    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+      // The URL constructor is available in all browsers that support SW.
+      const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
+      if (publicUrl.origin !== window.location.origin) {
+        // Our service worker won't work if PUBLIC_URL is on a different origin
+        // from what our page is served on. This might happen if a CDN is used to
+        // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
+        return;
+      }
+  
+      window.addEventListener('load', () => {
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+  
+        if (isLocalhost) {
+          // This is running on localhost. Lets check if a service worker still exists or not.
+          checkValidServiceWorker(swUrl);
+  
+          // Add some additional logging to localhost, pointing developers to the
+          // service worker/PWA documentation.
+          navigator.serviceWorker.ready.then(() => {
+            console.log(
+              'This web app is being served cache-first by a service ' +
+                'worker. To learn more, visit https://goo.gl/SC7cgQ'
+            );
+          });
+        } else {
+          // Is not local host. Just register service worker
+          registerValidSW(swUrl);
+        }
+      });
+    }
+  }
+  
+  function registerValidSW(swUrl) {
+    navigator.serviceWorker
+      .register(swUrl)
+      .then(registration => {
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === 'installed') {
+              if (navigator.serviceWorker.controller) {
+                // At this point, the old content will have been purged and
+                // the fresh content will have been added to the cache.
+                // It's the perfect time to display a "New content is
+                // available; please refresh." message in your web app.
+                console.log('New content is available; please refresh.');
+              } else {
+                // At this point, everything has been precached.
+                // It's the perfect time to display a
+                // "Content is cached for offline use." message.
+                console.log('Content is cached for offline use.');
+              }
+            }
+          };
+        };
+      })
+      .catch(error => {
+        console.error('Error during service worker registration:', error);
+      });
+  }
+  
+  function checkValidServiceWorker(swUrl) {
+    // Check if the service worker can be found. If it can't reload the page.
+    fetch(swUrl)
+      .then(response => {
+        // Ensure service worker exists, and that we really are getting a JS file.
+        if (
+          response.status === 404 ||
+          response.headers.get('content-type').indexOf('javascript') === -1
+        ) {
+          // No service worker found. Probably a different app. Reload the page.
+          navigator.serviceWorker.ready.then(registration => {
+            registration.unregister().then(() => {
+              window.location.reload();
+            });
+          });
+        } else {
+          // Service worker found. Proceed as normal.
+          registerValidSW(swUrl);
+        }
+      })
+      .catch(() => {
+        console.log(
+          'No internet connection found. App is running in offline mode.'
+        );
+      });
+  }
+  
+  export function unregister() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(registration => {
+        registration.unregister();
+      });
+    }
+  }
+  
